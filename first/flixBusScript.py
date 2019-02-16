ff571b7650f449b98fff78ae5b18fb777244b003
PiotrKedra <piotr.k9750@gmail.com>
2019-02-16 17:13:38 +0100

    G

--- first/flixBusScript.py
+++ first/flixBusScript.py
@@ -2,3 +2,21 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+
+
+url = "https://www.flixbus.pl/"
+r = requests.get(url)
+
+html = r.text
+
+
+print(r.text)
+
+print("DUPA #################")
+
+soup = BeautifulSoup(html, 'html.parser')
+
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
