05020a7ce1ab3e49a31de77420c9b51de546db7e
PiotrKedra <piotr.k9750@gmail.com>
2019-02-17 20:10:09 +0100

    progres

--- first/flixBusScript.py
+++ first/flixBusScript.py
@@ -1,22 +1,48 @@
+import re
+
 import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
 
 
-url = "https://www.flixbus.pl/"
+url = "https://shop.flixbus.pl/search?departureCity=1314&arrivalCity=1915&route=Arnhem-Kraków&rideDate=24.02.2019&adult=1&"
 r = requests.get(url)
 
 html = r.text
 
 
-print(r.text)
+#print(r.text)
 
 print("DUPA #################")
 
 soup = BeautifulSoup(html, 'html.parser')
 
 
-for link in soup.find_all('a'):
-    print(link.get('href'))
 
+# aux-id-interconnection
+
+for link in soup.find_all('div'):
+
+    try:
+        classes = link['class']
+
+        for clas in classes:
+            search = re.search(r'aux-id-interconnection', clas)
+
+            if search:
+                print(clas)
+                print(link)
+                print("DASDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    except:
+        continue
+
+
+
+# search?q=dupa&
+
+
+def departure_time(div: str):
+    flix_class_name = 'departure'
+
+    
